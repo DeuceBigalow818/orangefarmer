0740e4491967dce47c06ab7919d974242a1b341e
DeuceBigalow <sigulya.herz@gmail.com>
2026-03-03 16:26:28 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import OrangeFarmer from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <OrangeFarmer />
-  </React.StrictMode>
-);
